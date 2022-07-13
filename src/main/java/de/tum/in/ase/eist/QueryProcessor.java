372a42fc404895348a80f3374f8e51fe58fa5b8b
ge23gat <yuanyuan.yin@tum.de>
2022-07-13 14:59:02 +0200

    changen name

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -12,7 +12,7 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "MyTeam";
+           return "Yuan";
         } else { // TODO extend the programm here
             // Add a new comment, do not change anything
             return "";
